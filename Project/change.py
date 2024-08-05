2a688f94050d720e733442111bfa315a29aac475
Riley Adams <radam@lle.rochester.edu>
2024-08-05 13:58:01 -0400

    Adding change project

---
+++ Project/change.py
@@ -0,0 +1,10 @@
+priceStr = input("Price of item: ")
+amountGivenStr = input("Amount of money given: ")
+
+price = float(priceStr)
+amountGiven = float(amountGivenStr)
+
+changeGiven = 0
+
+while changeGiven > price - amountGiven:
+    pass
