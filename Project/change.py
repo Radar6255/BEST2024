12a979799ff4c27c3c6256b5ae709acbf1dcb61f
Riley Adams <radam@lle.rochester.edu>
2024-08-07 15:28:06 -0400

    Adding header to project

--- Project/change.py
+++ Project/change.py
@@ -1,3 +1,12 @@
+# -*-Python-*-
+#   FILE:       change.py
+#   AUTHOR:     Riley Adams
+#   DATE:       August 05, 2024
+#   PURPOSE:    A program to generate the required change to give someone
+#
+# Copyright 2024 University of Rochester. All rights reserved.
+#
+
 priceStr = input("Price of item: ")
 amountGivenStr = input("Amount of money given: ")
 
