b7641a5ec6d57440533f2462137ad30a8752f83c
Riley Adams <radam@lle.rochester.edu>
2024-08-07 11:36:47 -0400

    Adding data_types

---
+++ Demo/data_types.py
@@ -0,0 +1,24 @@
+# -*-Python-*-
+#   FILE:       data_types.py
+#   AUTHOR:     Riley Adams
+#   DATE:       August 07, 2024
+#   PURPOSE:    Program to show different data types
+#
+# Copyright 2024 University of Rochester. All rights reserved.
+#
+
+str_var = "Characters and numbers 0123456789 and symbols !@#$%^&*"
+print(str_var)
+
+int_var = 42
+print(int_var)
+
+float_var = 96.8
+print(float_var)
+
+bool_var = True
+print(bool_var)
+
+list_var = ["Mixed", 42, "List", True]
+
+print(list_var[0])
