f4a25e9fc096a5b9575ebe8b1136a04b271d0f05
Riley Adams <radam@lle.rochester.edu>
2024-08-07 15:25:47 -0400

    Merging data_types

--- Demo/data_types.py
+++ Demo/data_types.py
@@ -1,24 +1,66 @@
 # -*-Python-*-
-#   FILE:       data_types.py
-#   AUTHOR:     Riley Adams
-#   DATE:       August 07, 2024
-#   PURPOSE:    Program to show different data types
-#
+#      FILE :          data_types.py
+#      AUTHOR :        Robbie Kubiniec
+#      MODIFIED BY:    Riley Adams
+#      DATE:           July 18, 2024
+#      PURPOSE:        Demonstrate different types of data which python can store in "variables"
+# 
 # Copyright 2024 University of Rochester. All rights reserved.
 #
 
-str_var = "Characters and numbers 0123456789 and symbols !@#$%^&*"
-print(str_var)
 
-int_var = 42
-print(int_var)
+# When you 'declare' a variable, you put the name of the new variable on the left of the = sign, and the value of the variable on the right
+# ex.
+# name = value
 
-float_var = 96.8
-print(float_var)
+integer1 = 5
+integer2 = -20
+float1 = 4.2
+boolean1 = True
+string1 = "This is a string of characters"
+string2 = "20"
+string3 = "9.8"
+string4 = "False"
+string5 = "Characters and numbers 0123456789 and symbols !@#$%^&*"
+list1 = [ 5, 1, 3, 20, 0, 1, 30000 ]
+list2 = ["Mixed", 42, "List", True]
 
-bool_var = True
-print(bool_var)
+# You can print any variable to see its value
+print("An 'int' is an integer (whole) number, which can be positive or negative. Here are two examples:")
+print(integer1)
+print(integer2)
+print() # print an empty line for spacing the output
+print("A 'float' is any number, including decimal numbers. The name 'float' comes from 'floating point', which describes how a computer keeps track of the decimal point. Here's an example of a float:")
+print(float1)
+print()
+print("A 'boolean' value is always either True or False. Here's an example:")
+print(boolean1)
+print()
+print("A 'string' can be any bunch of characters. Strings are denoted by surrounding quotes. A string can even contain a value that looks like an int, boolean, or float, but if it's surrounded by quotes in your code, it's a string! Here are some examples:")
+print(string1)
+print(string2)
+print(string3)
+print(string4)
+print(string5)
+print()
+print("A 'list' is a collection of items of any data type. Each item in the list is called an 'element' of the list.  The elements are surrounded by [brackets] and separated by commas. You can mix and match anything you want in a list, but typically we make lists where all elements are of the same type. Here's an example:")
+print(list1)
+print(list2)
 
-list_var = ["Mixed", 42, "List", True]
+### Interactive ###
 
-print(list_var[0])
+# Add your own variables. Try the different data types, and print your variables to see their values
+
+# ______ = ???
+# print(______)
+
+
+
+
+
+
+
+
+
+
+
